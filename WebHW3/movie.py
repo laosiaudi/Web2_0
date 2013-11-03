7e45eda9f60df0aadf65c5175a238a7f9ad798b9
laosiaudi <laosiaudi@gmail.com>
2013-11-03 17:58:54 +0800

    modified

--- WebHW3/movie.py
+++ WebHW3/movie.py
@@ -1,4 +1,4 @@
-
+# -*-coding: utf-8 -*-
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -8,18 +8,32 @@ import os
 from tornado.options import define, options
 define("port", default=8883, help="run on the given port", type=int)
 class movie:
-    def __init__(self, info, overview, alist):
+    def __init__(self, info, overview, alist, pic):
         self.infolist = []
-        self.reviewgroup = []
+        self.reviewgroupl = []
+        self.reviewgroupr = []
         self.overview = {}
+        self.pic = pic
         for line in info:
             self.infolist.append(line)
         for lint in overview:
             info = lint.split(':')
             self.overview[info[0]] = info[1]
-        for item in alist:
-            for line in item:
-                self.reviewgroup.append(line)
+        for i in range(len(alist)):
+            if i < len(alist)/2:
+                content = []
+                for line in alist[i]:
+                    line = line[:-1]
+                    content.append(line)
+
+                self.reviewgroupl.append(content)
+            else:
+                content = []
+                for line in alist[i]:
+                    line = line[:-1]
+                    content.append(line)
+                self.reviewgroupr.append(content)
+            
 
         
 
@@ -44,8 +58,8 @@ class MainHandler(tornado.web.RequestHandler):
         overviewpath = '/generaloverview.txt'
         allpathInfo = dire + input + infopath
         allpathOver = dire + input + overviewpath
-        f1= open(allpathInfo,'r')
-        f2 = open(allpathOver,'r')
+        filetokenInfo= open(allpathInfo,'r')
+        filetokenOver = open(allpathOver,'r')
         review = []
         filename = ['/review1.txt', '/review2.txt', '/review3.txt', '/review4.txt', '/review5.txt', '/review6.txt', '/review7.txt', '/review8.txt', '/review9.txt', '/review10.txt', '/review11.txt', '/review12.txt']
         files = os.listdir(dire + input)
@@ -55,8 +69,10 @@ class MainHandler(tornado.web.RequestHandler):
             filenames = dire + input + filename[i]
             ftoken = open(filenames,'r')
             review.append(ftoken)
+            
 
-        objectM = movie(f1, f2, review)
+        pic = input + '.png'
+        objectM = movie(filetokenInfo, filetokenOver, review, pic)
 
 
         self.render("template.html", movie = objectM)
@@ -65,7 +81,6 @@ class MainHandler(tornado.web.RequestHandler):
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    """app = tornado.web.Application(Application())"""
     http_server=tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
