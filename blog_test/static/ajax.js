0fbec961e59fb3e6815c64d7d2b49ffe22c8ba68
laosiaudi <laosiaudi@gmail.com>
2013-12-24 11:58:49 +0800

    pymongo support and ajax refresh

--- blog_test/static/ajax.js
+++ blog_test/static/ajax.js
@@ -3,7 +3,7 @@
 // FILE:     ajax.js
 // 2013 @laosiaudi All rights reserved
 // CREATED:  2013-12-23 18:22:48
-// MODIFIED: 2013-12-24 11:32:23
+// MODIFIED: 2013-12-24 11:55:05
 
 window.onload = function(){
   $("commit").addEventListener("onclick",commit);
@@ -14,13 +14,13 @@ function commit(){
   var form = $("form");
   var input = form['comment_submit'];
   var data = $(input).getValue();
-  new Ajax.Updater('form','/some',{
+  new Ajax.Updater('comment_block','/some',{
       method:'post',
       parameters:{'comment_submit':data},
       onSuccess:function (ajax){
         var form = $("form");
         form.reset();
       },
-      insertion:'top'
+      insertion:'bottom'
   });
 }
